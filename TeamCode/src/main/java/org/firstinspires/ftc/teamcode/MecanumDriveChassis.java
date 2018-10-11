14e7d6eccd4a7efe553353ed11515790aa1a5802
9013Matrix <9013Matrix@gmail.com>
2018-10-11 16:34:34 -0400

    practice teleop fixed (control scheme reversal now fixed)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveChassis.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class MecanumDriveChassis
+{
+  private DcMotor frontRight = null;
+  private DcMotor frontLeft = null;
+  private DcMotor rearRight = null;
+  private DcMotor rearLeft = null;
+  
+  
+  MecanumDriveChassis( DcMotor FR, DcMotor FL, DcMotor RR, DcMotor RL)
+  {
+   this.frontRight = FR;
+   this.frontLeft = FL;
+   this.rearRight = RR;
+   this.rearLeft = RL;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+}
