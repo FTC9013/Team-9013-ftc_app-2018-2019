e6ec272e9e2f9781d4dcf0c71fa61b64d2f369f2
9013Matrix <9013Matrix@gmail.com>
2018-11-02 17:57:42 -0400

    daily work on drive chassis.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveChassis.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDriveChassis.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.Range;
+import java.util.Collections;
+import java.util.List;
 
 public class MecanumDriveChassis
 {
@@ -12,11 +14,18 @@ public class MecanumDriveChassis
   private DcMotor rightRearDrive = null;
 
 
-  private double leftFrontDriveSpeed;
-  private double leftRearDriveSpeed;
-  private double rightFrontDriveSpeed;
-  private double rightRearDriveSpeed;
-
+  private static double leftFrontDriveSpeed;
+  private static double leftRearDriveSpeed;
+  private static double rightFrontDriveSpeed;
+  private static double rightRearDriveSpeed;
+  
+  // Robot speed [-1, 1].
+  private static double vD;
+  // Robot angle while moving [0, 2pi].
+  private static double thetaD;
+  // Speed for changing direction [-1, 1].
+  private static double vTheta;
+  
   // comment.....
   
   MecanumDriveChassis(HardwareMap hardwareMap)
@@ -82,4 +91,120 @@ public class MecanumDriveChassis
     rightRearDrive.setPower(rightRearDriveSpeed);
     leftRearDrive.setPower(leftRearDriveSpeed);
   }
+  
+//import java.util.Arrays;
+
+
+/**
+ * Mecanum wheel drive calculations.
+ * Input controls:
+ *   V_d = desired robot speed.
+ *   theta_d = desired robot velocity angle.
+ *   V_theta = desired robot rotational speed.
+ *
+ *  Example:
+ *    // Convert joysticks to wheel powers.
+ *    Mecanum.Wheels wheels = Mecanum.motionToWheels(
+ *        Mecanum.joystickToMotion(
+ *            gamepad1.left_stick_x, gamepad1.left_stick_y,
+ *            gamepad1.right_stick_x, gamepad1.right_stick_y));
+ *    // Set power on the motors.
+ *    frontLeftMotor.setPower(wheels.frontLeft);
+ */
+public class Mecanum {
+
+
+  /**
+   * Gets the motion vector from the joystick values.
+   * @param leftStickX The left joystick X.
+   * @param leftStickY The left joystick Y.
+   * @param rightStickX The right joystick X.
+   * @param rightStickY The right joystick Y.
+   * @return The Mecanum motion vector.
+   */
+  public static Motion joystickToMotion(double leftStickX,
+                                        double leftStickY,
+                                        double rightStickX,
+                                        double rightStickY) {
+    double vD = Math.min(Math.sqrt(Math.pow(leftStickX, 2) +
+                                       Math.pow(leftStickY, 2)),
+        1);
+    double thetaD = Math.atan2(-leftStickX, -leftStickY);
+    double vTheta = -rightStickX;
+    return new Motion(vD, thetaD, vTheta);
+  }
+
+  /**
+   * Mecanum wheels, used to get individual motor powers.
+   */
+  public static class Wheels {
+    // The mecanum wheels.
+    public final double frontLeft;
+    public final double frontRight;
+    public final double backLeft;
+    public final double backRight;
+
+    /**
+     * Sets the wheels to the given values.
+     */
+    public Wheels(double frontLeft, double frontRight,
+                  double backLeft, double backRight) {
+      List<Double> powers = Arrays.asList(frontLeft, frontRight,
+          backLeft, backRight);
+      clampPowers(powers);
+
+      this.frontLeft = powers.get(0);
+      this.frontRight = powers.get(1);
+      this.backLeft = powers.get(2);
+      this.backRight = powers.get(3);
+    }
+
+    /**
+     * Scales the wheel powers by the given factor.
+     * @param scalar The wheel power scaling factor.
+     */
+    public Wheels scaleWheelPower(double scalar) {
+      return new Wheels(frontLeft * scalar, frontRight * scalar,
+          backLeft * scalar, backRight * scalar);
+    }
+  }
+
+  /**
+   * Gets the wheel powers corresponding to desired motion.
+   * @param motion The Mecanum motion vector.
+   * @return The wheels with clamped powers. [-1, 1]
+   */
+  public static Wheels motionToWheels(Motion motion) {
+    double vD = motion.vD;
+    double thetaD = motion.thetaD;
+    double vTheta = motion.vTheta;
+
+    double frontLeft = vD * Math.sin(-thetaD + Math.PI / 4) - vTheta;
+    double frontRight  = vD * Math.cos(-thetaD + Math.PI / 4) + vTheta;
+    double backLeft = vD * Math.cos(-thetaD + Math.PI / 4) - vTheta;
+    double backRight = vD * Math.sin(-thetaD + Math.PI / 4) + vTheta;
+    return new Wheels(frontLeft, frontRight,
+        backLeft, backRight);
+  }
+
+  /**
+   * Clamps the motor powers while maintaining power ratios.
+   * @param powers The motor powers to clamp.
+   */
+  private static void clampPowers(List<Double> powers) {
+    double minPower = Collections.min(powers);
+    double maxPower = Collections.max(powers);
+    double maxMag = Math.max(Math.abs(minPower), Math.abs(maxPower));
+
+    if (maxMag > 1.0) {
+      for (int i = 0; i < powers.size(); i++) {
+        powers.set(i, powers.get(i) / maxMag);
+      }
+    }
+  }
+}
+
+
+
+
 }
